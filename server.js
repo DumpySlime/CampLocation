cc2cb67aa3eba62eae9fd1bc99bbf12db425e2d3
DumpySlime <129273682+DumpySlime@users.noreply.github.com>
2024-11-29 20:16:43 +0800

    updated package.json, server.js added serarch.ejs

--- server.js
+++ server.js
@@ -1,4 +1,37 @@
 // JavaScript source code
 const express = required('express');
 const app = express();
+const mongoose = required('mongoose')
 const { MongoClient } = required('mongodb');
+const path = required('path');
+
+const Campsite = required('./models/campsite_data');
+
+const uri = 'mongodb+srv://brianto:admin@cluster0.o1tli.mongodb.net/campsite_data?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri);
+
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+async function main() {
+    try {
+        await mongoose.connect(uri);
+        console.log('Mongoose Connected!');
+    } catch (error) {
+        console.error('Mongoose connection error:')
+    }
+    await client.connect();
+    console.log('Client Connected')
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+main()
+    .then(console.log())
+    .catch((err) => console.log(err))
+    .finally()
